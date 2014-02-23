ffb169257948b7cd9172c74f4d51ab0bdcc3cf5c
Frans Den <fransden@gmail.com>
2014-02-23 16:48:31 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rortest7::Application.config.secret_key_base = '5028dc2cbdccc83e851d44d495aa97b4bf02767e6659f2f8114fb876be961a34ce0bcc76a89f9702dda1ee42c452f2aa9860118d16050caf04722ed28b7f60d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rortest7::Application.config.session_store :cookie_store, key: '_rortest7_session'
